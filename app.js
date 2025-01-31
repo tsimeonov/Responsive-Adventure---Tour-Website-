4ef18ced4ed95c0f035403ed49bea99076ca610c
tsimeonov <tsimeonov12@gmail.com>
2025-01-31 16:46:06 +0100

    open the menucloseMenu function

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const openMenu = () => {
   document.querySelector("aside").className = "active";
 };
 
+const closeMenu = () => {
+  document.querySelector("backdrop").className = "backdrop";
+  document.querySelector("aside").className = "";
+};
+
 document.getElementById("menuBtn").onclick = (e) => {
   e.preventDefault();
 };
