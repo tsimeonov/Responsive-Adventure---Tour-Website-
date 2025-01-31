25f8bdc84688525cb75fa730a17e8fa8b1405a0b
tsimeonov <tsimeonov12@gmail.com>
2025-01-31 17:08:53 +0100

    update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const openMenu = () => {
-  document.querySelector("backdrop").className = "backdrop active";
+  document.querySelector(".backdrop").className = "backdrop active";
   document.querySelector("aside").className = "active";
 };
 
