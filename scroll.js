acfc35beb9c5aab19dac20bde62bcb44ee26ce0e
tsimeonov <tsimeonov12@gmail.com>
2025-02-11 19:59:07 +0100

    add a forEach

--- scroll.js
+++ scroll.js
@@ -5,5 +5,9 @@ document.addEventListener("scroll", () => {
     window.pageXOffset - document.lastScrollPosition > 0 ? "down" : "up";
   const section = [...document.querySelectorAll("section")];
 
+  sections.forEach((section) => {
+    document.lastCentered = index;
+  });
+
   document.lasScrollPosition = window.pageXOffset;
 });
