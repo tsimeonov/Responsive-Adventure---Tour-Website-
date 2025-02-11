57117e87c50c23444dab22160f050c34b9d556fa
tsimeonov <tsimeonov12@gmail.com>
2025-02-11 20:04:27 +0100

    update the code

--- scroll.js
+++ scroll.js
@@ -4,9 +4,15 @@ document.lastCentered = 0;
 document.addEventListener("scroll", () => {
   const direction =
     window.pageXOffset - document.lastScrollPosition > 0 ? "down" : "up";
-  const section = [...document.querySelectorAll("section")];
+  const sections = [...document.querySelectorAll("section")];
 
-  sections.forEach((section) => {
+  const destIndex =
+    direction === "up" ? document.lastCentered - 1 : document.lastCentered;
+  if (destIndex >= 0 && destIndex < sections.length) {
+    window.scroll(0, sections[destIndex].offsetTop);
+  }
+
+  sections.forEach((section, index) => {
     document.lastCentered = index;
   });
 
